0dfdded973065063b51bd1002a97565736d688bd
John Doe <john@sample.com>
2017-01-19 10:05:07 +0000

    commit to git for OpenGI eBroker4

--- src/actions/eBroker4/autocompleteinput.java
+++ src/actions/eBroker4/autocompleteinput.java
@@ -1,33 +1,35 @@
 package actions.eBroker4;
 
-
 import java.util.*;
-
+import java.lang.*;
 import actions.selenium.utils.Elements;
 import actions.selenium.Browser;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.*;
 
-class clickoncheckbox{
+
+class autocompleteinput{
     public void run(HashMap<String, String> params){
         
     if (params.get("selectBy").equals("css")){            
-                        
-            WebElement select = Browser.Driver.findElement(By.cssSelector(params.get("selectByvalue")));
+               
+        	if (params.get("inputValue").length()>0)
+            {
+            WebElement input = Browser.Driver.findElement(By.cssSelector(params.get("selectinputByvalue")));
+        	input.sendKeys(params.get("inputValue"));
+            }
+        
+        	WebElement select = Browser.Driver.findElement(By.cssSelector(params.get("autoListname")));
             
             List<WebElement> options = select.findElements(By.tagName(params.get("tagname")));
                   
                 for (WebElement option : options) 
                 {       
-                        if(params.get("Value").equals("Yes"))
+                         if(params.get("inputValue").equals(option.getText().trim()))
                         {		
-                          if (!option.isSelected())
-                          {
                               option.click();  
-                              break;
-                          }
-                           
+                              break;                          
                        	}
                             
             	}    
